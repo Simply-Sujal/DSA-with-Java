fd121e882df642ea1b12ed8859783760498fca82
Sujal <sujal.kumar311@gmail.com>
2022-02-20 14:29:44 +0700

    Stack and Queues

---
+++ Stack and Queue/Inbuiltexamples.java
@@ -0,0 +1,46 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+// Here we will learn about how to write code for Stack Data Structure and Queue Data Structure
+
+public class Inbuiltexamples{
+    public static void main(String[] args) {
+        // Stack<Integer> stack = new Stack<>();
+        // stack.push(32);
+        // stack.push(100);
+        // stack.push(45);
+        // stack.push(5);
+        // stack.push(76);
+
+        // System.out.println(stack.pop());
+        // System.out.println(stack.pop());
+        // System.out.println(stack.pop());
+        // System.out.println(stack.pop());
+        // System.out.println(stack.pop());
+        // System.out.println(stack.pop()); -> this will give us an error of EmptyStackException 
+       
+
+        // Queue<Integer> queue = new LinkedList<>();
+        // queue.add(100);
+        // queue.add(34);
+        // queue.add(8);
+        // queue.add(1);
+        // queue.add(67);
+
+        // System.out.println(queue.peek());
+        // System.out.println(queue.remove());
+
+
+        // Deque<Integer> deque = new ArrayDeque<>();
+        // deque.add(56);
+        // deque.addFirst(67);
+        // deque.addLast(99);
+        // deque.add(34);
+        // deque.add(1);
+        // deque.removeFirst();
+        // System.out.print(deque);
+    }
+}
