a774f6e0ec595a5af726472e9128914169235325
Sujal <sujal.kumar311@gmail.com>
2022-03-14 20:31:12 +0700

    Few qst.

---
+++ Maths For DSA/Q6.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Q6 {
+    public static void main(String[] args) {
+       System.out.print(1+2+"3");
+       System.out.print(1+"2"+3);
+       System.out.print("1"+2+3);
+    }
+}
+
+
+// class A{
+//     int i ;
+//     void display(){
+//         System.out.println(i);
+//     }
+// }
+
+// class B extends A{
+//     int j;
+//     void display(){
+//         System.out.println(j);
+//     }
+// }
---
+++ Maths For DSA/Qst1.java
@@ -0,0 +1,8 @@
+public class Qst1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 16;
+        System.out.println(a&b / 2*5+7);
+    }
+}
+
